dc67a9ed4c7a99e9dc5da93de96aeae751ce9bee
Glauber <grobis_mn@hotmail.com>
2017-09-06 18:54:17 -0300

    COnexao com o banco

---
+++ Bitfour.lojaVirtual.Dominio/Repositorio/EfbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bitfour.lojaVirtual.Dominio.Entidades;
+
+namespace Bitfour.lojaVirtual.Dominio.Repositorio
+{
+    public class EfbContext : DbContext
+    {
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
